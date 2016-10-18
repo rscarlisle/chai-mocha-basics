9fd60ec93ea5bbe5803a5d32b7a44d2113c79e42
Randy Carlisle <cisolutionsinc@gmail.com>
2016-10-17 17:48:03 -0700

    Initial tests with chai and mocha

---
+++ test/classNameTest.js
@@ -0,0 +1,36 @@
+var assert = chai.assert;
+
+describe('addClass', function() {
+  it('should add class to element', function() {
+    var element = { className: '' };
+
+    addClass(element, 'test-class');
+
+    assert.equal(element.className, 'test-class');
+  });
+
+  it('should not add a class which already exists', function () {
+    var element = { className: 'exists' };
+    addClass(element, 'exists');
+
+    var numClasses = element.className.split(' ').length;
+    assert.equal(numClasses, 1);
+  });
+
+  it('should append new class after existing one', function () {
+    var element = { className: 'exists' };
+
+    addClass(element, 'new-class');
+
+    var classes = element.className.split(' ');
+    assert.equal(classes[1], 'new-class');
+  });
+});
+
+// var expect = require('chai').expect;
+
+// describe('A basic test', function () {
+//     it('should pass when everything is ok', function () {
+//         expect(true).to.be.true;
+//     });
+// });
